f7f6b903ab42c52ae444f50929282752f1b6527b
nek00oo <yakovenkovalera2017@yandex.ru>
2025-03-22 00:17:05 +0300

    added type pagination.ts

---
+++ src/common/types/pagination.ts
@@ -0,0 +1,14 @@
+export interface PaginatedResponse<T> {
+    data: T[];
+    meta: {
+        total: number;
+        page: number;
+        limit: number;
+        totalPages: number;
+    };
+}
+
+export interface PaginationParams {
+    page: number;
+    limit: number;
+}
